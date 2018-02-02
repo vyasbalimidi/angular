48b1aa87acb672c103c83c848eed29c1756b7770
vyasbalimidi <vbalimi@capelit51748832.inedc.corpintra.net>
2018-02-02 17:04:08 +0530

    Filtering of ECOs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EcoComponent } from './eco/eco.component';
 import { EcoService } from './eco.service';
+import { EcoFilterPipe } from './eco-filter.pipe';
 
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EcoComponent
+    EcoComponent,
+    EcoFilterPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [EcoService],
---
+++ src/app/eco-filter.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Eco } from './Eco';
+
+@Pipe({
+    name: 'filter'
+})
+
+export class EcoFilterPipe implements PipeTransform {
+    transform(ecos: Eco[], searchText: string): Eco[] {
+        if (!ecos) return [];
+        if (!searchText) return ecos;
+
+        let searchTextLC = searchText.toLowerCase();
+        return ecos.filter(eco => {
+            return eco.eco_number.toLowerCase().includes(searchTextLC);
+        });
+    }
+}
